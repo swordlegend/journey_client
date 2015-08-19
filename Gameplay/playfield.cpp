4c1d101d22ae2a402d726f7d697186c8338e0041
SYJourney <ysfoig2153tg@hotmail.de>
2015-08-19 20:50:51 +0200

    Update playfield.cpp

--- Gameplay/playfield.cpp
+++ Gameplay/playfield.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
 // This file is part of the Journey MMORPG client                           //
-// Copyright © 2015 SYJourney                                               //
+// Copyright Â© 2015 SYJourney                                               //
 //                                                                          //
 // This program is free software: you can redistribute it and/or modify     //
 // it under the terms of the GNU Affero General Public License as           //
@@ -23,16 +23,59 @@ namespace gameplay
 {
 	bool playfield::moveup()
 	{
-		pair<int, string> warpinfo = m_map.getportal();
-		if (warpinfo.first == m_map.getid())
+		pair<int, string> warpinfo = portals.getportal(playerchar.getposition());
+		if (warpinfo.first == map_info.getid())
 		{
-			playerchar.setposition(m_map.getspawnp(warpinfo.second));
-			return true;
+			vector2d spawnpos = portals.getspawnpoint(warpinfo.second);
+			playerchar.setposition(spawnpos);
 		}
 		else if (warpinfo.first != 0)
 		{
 			packet_c.changemap(false, warpinfo.first, warpinfo.second, false);
 			return false;
+		}
+		return true;
+	}
+
+	void playfield::useattack(int skillid)
+	{
+		if (skillid == -1)
+		{
+			bool left = playerchar.getleft();
+
+			pair<vector2d, vector2d> attackrange;
+			vector2d range = vector2d(50, 80);
+			vector2d playerpos = playerchar.getposition();
+			if (left)
+			{
+				attackrange.first = playerpos - range;
+				attackrange.second = playerpos;
+			}
+			else
+			{
+				attackrange.first = playerpos;
+				attackrange.second = playerpos + range;
+			}
+
+			int basedamage = playerchar.getstats()->basedamage;
+
+			attackinfo attack;
+			attack.mastery = 0.5f; //todo: get from char
+			attack.skill = 0;
+			attack.maxattacked = 1;
+			attack.display = 5;
+			attack.charge = 0;
+			attack.speed = 2; //todo: wep speed of char
+			attack.direction = (left) ? 1 : 0;
+			attack.numdamage = 1;
+			attack.stance = 1; //maybe body animation?
+
+			map_objects.sendattack(&attack, basedamage, attackrange);
+			packet_c.close_attack(attack);
+		}
+		else
+		{
+			//get skill
 		}
 	}
 
@@ -41,8 +84,19 @@ namespace gameplay
 		if (step == GST_GAME)
 		{
 			view.update(vector2d(408, (624 / 1.5)) - playerchar.getposition());
-			m_map.draw(target, view.getposition());
+
+			backgrounds.drawbackgrounds(target, view.getposition());
+			for (int i = 0; i < 5; i++)
+			{
+				maplayers[i].draw(target, view.getposition());
+			}
+			map_objects.draw(target, view.getposition());
 			playerchar.draw(target, view.getposition());
+			portals.draw(target, view.getposition());
+			maplayers[5].draw(target, view.getposition());
+			maplayers[6].draw(target, view.getposition());
+			maplayers[7].draw(target, view.getposition());
+			backgrounds.drawforegrounds(target, view.getposition());
 		}
 	}
 
@@ -50,24 +104,16 @@ namespace gameplay
 	{
 		if (step == GST_GAME)
 		{
+			backgrounds.update();
+			map_objects.update();
 			playerchar.update();
-			m_map.update(playerchar.getposition());
+			portals.update(playerchar.getposition());
+
+			for (int i = 0; i < 8; i++)
+			{
+				maplayers[i].update();
+			}
 		}
-	}
-
-	void playfield::changemap(maplemap newmap, char pid)
-	{
-		step = GST_TRANSITION;
-
-		m_map = newmap;
-		playerchar.setmap(&m_map);
-
-		vector2d startpos = m_map.getspawnp(pid);
-		playerchar.setposition(startpos);
-		view.setposition(vector2d(816 / 2, (624 / 1.5)) - startpos);
-		view.setbounds(m_map.getwalls(), m_map.getborders());
-
-		step = GST_GAME;
 	}
 
 	void playfield::setplayer(player plchar)
@@ -76,5 +122,83 @@ namespace gameplay
 
 		m_account.clear();
 		playerchar = plchar;
+	}
+
+	void playfield::setfield(nxprovider* provider, int mapid, char pid)
+	{
+		step = GST_TRANSITION;
+
+		map_objects.clear();
+		portals.clear();
+
+		provider->setcmode(CM_MAP);
+		nl::nx::view_file("Map");
+
+		string fullname;
+		string strid = to_string(mapid);
+		char extend = 9 - strid.length();
+		for (char i = 0; i < extend; i++)
+		{
+			fullname.append("0");
+		}
+		node mapdata = nl::nx::nodes["Map"].resolve("Map/Map" + to_string(mapid / 100000000) + "/" + fullname.append(strid) + ".img");
+
+		map_info = mapinfo(mapid, mapdata.resolve("info"));
+		footholds = footholdtree(mapdata.resolve("foothold"));
+
+		nl::node portalnodes = mapdata.resolve("portal");
+		for (nl::node portalnd = portalnodes.begin(); portalnd != portalnodes.end(); portalnd++)
+		{
+			char pid = static_cast<char>(stoi(portalnd.name()));
+
+			portaltype ptype = static_cast<portaltype>(portalnd.resolve("pt").get_integer());
+			string pname = portalnd.resolve("pn").get_string();
+			vector2d ppos = vector2d(static_cast<int>(portalnd.resolve("x").get_integer()), static_cast<int>(portalnd.resolve("y").get_integer()));
+			int targetid = static_cast<int>(portalnd.resolve("tm").get_integer());
+			string targetpname = portalnd.resolve("tn").get_string();
+
+			animation anim;
+
+			nl::node pnode = nl::nx::nodes["Map"].resolve("MapHelper.img/portal/game");
+			switch (ptype)
+			{
+			case PT_REGULAR:
+				anim = provider->loadanimation(pnode.resolve("pv"));
+				break;
+			case PT_HIDDEN:
+			case PT_SCRIPTED_HIDDEN:
+				anim = provider->loadanimation(pnode.resolve("ph/default/portalContinue"));
+				break;
+			}
+
+			portals.addportal(pid, portal(ptype, pname, targetid, targetid == mapid, targetpname, anim, ppos));
+		}
+
+		nl::nx::view_file("Back");
+
+		backgrounds = mapbackgrounds(provider, mapdata.resolve("back"), map_info.getwalls(), map_info.getborders());
+
+		nl::nx::unview_file("Back");
+
+		nl::nx::view_file("Tile");
+		nl::nx::view_file("Obj");
+
+		for (int i = 0; i < 8; i++)
+		{
+			maplayers[i] = maplayer(provider, mapdata.resolve(to_string(i)));
+		}
+
+		nl::nx::unview_file("Tile");
+		nl::nx::unview_file("Obj");
+		nl::nx::unview_file("Map");
+		provider->unlock();
+
+		vector2d startpos = portals.getspawnpoint(pid);
+		playerchar.setfh(&footholds);
+		playerchar.setposition(startpos);
+		view.setposition(vector2d(816 / 2, (624 / 1.5)) - startpos);
+		view.setbounds(map_info.getwalls(), map_info.getborders());
+
+		step = GST_GAME;
 	}
 }
